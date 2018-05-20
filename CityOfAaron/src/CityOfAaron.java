7f0bd34516749e8f33fd0119d1040bb413f1e2fa
cfull <cfull@DESKTOP-LPM7SAR>
2018-05-19 22:57:44 -0600

    5-19-18 Carson Individual 1

--- CityOfAaron/src/CityOfAaron.java
+++ CityOfAaron/src/CityOfAaron.java
@@ -10,6 +10,8 @@ import model.Storehouse;
 import model.Map;
 import model.Point;
 import model.Location;
+import model.Animal;
+import model.Author;
 
 /**
  *
@@ -49,5 +51,27 @@ public class CityOfAaron {
         theLocation.setGameTips(args);
         theLocation.setMapSymbol("$");
         theLocation.setName("Name of Location");
+        
+        //Test the Storehouse class.
+        Storehouse[] theStorehouse = new Storehouse[];
+        theStorehouse.setAuthors(args);
+        theStorehouse.setAnimals(args);
+        theStorehouse.setTools(args);
+        theStorehouse.setProvisions(args);
+        
+        //Test the Player class.
+        Player thePlayer = new Player();
+        thePlayer.setName("Name of the player");
+        
+        //Test the Animal class.
+        Animal theAnimal = new Animal();
+        theAnimal.setName("Name of an animal");
+        theAnimal.setAge(6);
+        
+        //Test the Author class.
+        Author theAuthor = new Author();
+        theAuthor.setName("Name of the author");
+        theAuthor.setTitle("Title of the author");
     }
 }
+ 
