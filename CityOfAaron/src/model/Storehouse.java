fe87cc05292be0494ce2ff7721c220668dff60d8
cfull <cfull@DESKTOP-LPM7SAR>
2018-05-22 17:02:28 -0600

    Revert "5-22-18 Carson Individual 1"
    This reverts commit e9d3323ddd6a7a0e8f070105fd50670ae3316852.

--- CityOfAaron/src/model/Storehouse.java
+++ CityOfAaron/src/model/Storehouse.java
@@ -1,67 +1,22 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this templa te file, choose Tools | Templates
+ * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package model;
 
 /**
  *
- * @author Carson Fullmer
+ * @author Zac Allen
  */
 public class Storehouse {
-    
-    
 
-    private Author[] authors;
-    private InventoryItem[] animals;
-    private InventoryItem[] tools;
-    private InventoryItem[] provisions;
-    
     public Storehouse() {
         //Empty constructor for JavaBeans
     }
     
-    
-    public Author[] getAuthors() {
-        return authors;
-    }
-    
-    public void setAuthors(Author[] authors) {
-        this.authors = authors;
-    }
-    public  InventoryItem[] getAnimals() {
-        return animals;
-    }
-    
-    public void setAnimals(InventoryItem[] animals) {
-        this.animals = animals;
-    }
-    
-    public InventoryItem[] getTools() {
-        return tools;
-    }
-    
-    public void setTools(InventoryItem[] tools) {
-        this.tools = tools;
-    }
-
-    public InventoryItem[] getProvisions() {
-        return provisions;
-    }
-
-    public void setProvisions(InventoryItem[] provisions) {
-        this.provisions = provisions;
-    }
-    
-    
     @Override
     public String toString(){
-        return "Storehouse {"
-                + "authors=" + authors
-                + "animals=" + animals
-                + "tools=" + tools
-                + "provisions=" + provisions
-                + "}";
+        return ("Class coming soon.");
     }
 }
