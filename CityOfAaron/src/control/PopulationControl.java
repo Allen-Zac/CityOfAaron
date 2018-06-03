a230cbed55541ec0a8d5c74bea11ecde8995188f
cfull <cfull@DESKTOP-LPM7SAR>
2018-06-03 00:29:42 -0600

    6/2/2-18 PopulationControl testing

--- CityOfAaron/src/control/PopulationControl.java
+++ CityOfAaron/src/control/PopulationControl.java
@@ -10,13 +10,39 @@ import java.util.Random;
  * @author Ginger
  */
 public class PopulationControl {
+    
+    private static int getRandomInRange(int min, int max) {
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
     public int numToFeed(int wheatOwned, int wheatFed) {
         
         if (wheatOwned < 0 || wheatFed < 0 || wheatFed > wheatOwned) {
             return -1;
         }
-        wheatOwned = wheatOwned - wheatFed;
-        int peopleFed = wheatFed / 10;
+        int peopleFed = wheatFed / 20;
         return peopleFed;
     }
+    
+    public int popDeath(int numPop, int wheatFed, int wheatOwned) {
+        int numToFeed = wheatFed / 20;
+        int popDeath = numPop - numToFeed;
+        
+        if (numPop < 0 || wheatFed < 0 || wheatOwned < 0 || 
+                wheatFed > wheatOwned) {
+                return -1;
+        }
+        return popDeath;
+    }
+    
+    public int popGrowth(int r, int numPop) {
+       if (r < 0 || numPop < 0 || r < 1 || r > 5) {
+           return -1;
+       }
+       int percentGrowth = getRandomInRange(3, 3) / 100;
+       int popGrowth = percentGrowth * numPop;
+       return popGrowth;
+    }
+    
 }
+
