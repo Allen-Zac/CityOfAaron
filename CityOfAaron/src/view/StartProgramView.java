38fb39ad01627ba38cad69a8c54de685ec85f332
cfull <cfull@DESKTOP-LPM7SAR>
2018-06-07 17:03:15 -0600

    6/6/2018 Lesson 7 Started Team Assignment

---
+++ CityOfAaron/src/view/StartProgramView.java
@@ -0,0 +1,149 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+import java.util.Scanner;
+/**
+ *
+ * @author cfull
+ */
+
+   
+public class StartProgramView {
+    
+    
+    /**
+     * The message that will be displayed by this view.
+     */
+    protected String message;
+    
+    /**
+     * Constructor
+     */
+    public StartProgramView(){
+        
+        message = "Welcome to our City called Aaron,. You is da king bruh! You gotta buy the land, sell the land, plan dat wheat so your peeps don't die. " 
+                + "Since you're the man in charge, you gotta make them pay thier taxes/tithes, or else they going straigh to hell."
+                + "If your people don't get enough grub, they gon' die and you got no mo slaves."
+                + "Them dang ol' rats gon' eat your wheat, so watch out, ya hear!"
+                + "responsibility(what we are doing)"
+                + "rat danger";
+                
+    }
+    
+    
+    /**
+     * Get the user's input. Keep prompting them until they enter a value.
+     * @param prompt
+     * @param allowEmpty - determine whether the user can enter no value (just a return key)
+     * @return 
+     */
+    protected String getUserInput(String prompt, boolean allowEmpty){
+        
+        Scanner keyboard = new Scanner(System.in);
+        String input = "";
+        boolean inputReceived = false;
+        
+        while(inputReceived == false){
+            
+            System.out.println(prompt);
+            input = keyboard.nextLine();
+            
+            // Make sure we avoid a null-pointer error.
+            if (input == null){
+                input = "";
+            }
+            
+            // Trim any trailing whitespace, including the carriage return.
+            input = input.trim();
+            
+            if (input.equals("") == false || allowEmpty == true){
+                inputReceived = true;
+            }
+        }
+        
+        return input;
+    }
+    
+    
+    /**
+     * An overloaded version of getUserInput that sets allowEmpty to false so we don't have 
+     * to type it ourselves.
+     * @param prompt
+     * @return 
+     */
+    protected String getUserInput(String prompt){
+        return getUserInput(prompt, false);
+    }
+    
+    /**
+     * Get the set of inputs from the user.
+     * @return 
+     */
+    public String[] getInputs() {
+        
+       //No other inputs
+        
+        return null;
+    }
+    
+    
+    /**
+     * Perform the action indicated by the user's input.
+     * @param inputs
+     * @return true if the view should repeat itself, and false if the view
+     * should exit and return to the previous view.
+     */
+    public boolean doAction(String[] inputs){
+       
+        StartMainMenuView();
+        
+        //Return false so whoever calld us doesn't call us again.
+        return false;
+    }
+    
+    
+    /**
+     * Control this view's display/prompt/action loop until the user
+     * chooses and action that causes this view to close.
+     */
+    public void displayView(){
+        
+        boolean keepGoing = true;
+        
+        while(keepGoing == true){
+            
+            System.out.println(message);
+            String[] inputs = getInputs();
+            keepGoing = doAction(inputs);
+        }
+    }
+    
+    
+    // Define your action handlers here. These are the methods that your doAction()
+    // method will call based on the user's input. We don't want to do a lot of 
+    // complex game stuff in our doAction() method. It will get messy very quickly.
+    
+    
+    private boolean someActionHandler(){
+        // Define whatever code you need here to accomplish the action.
+        // You can make this a void method if you want. Whatever you need 
+        // here, you are free to do.
+        //
+        // Generally, though, this is where you will call into your Control
+        // classes to do the work of the application.
+        
+        return true;
+    }
+    
+    private void startMainMenuView() {
+        MainMenuView mainMenu = new MainMenuView();
+        mainMenu.displayView();
+    }
+}
+
+    
+
+
